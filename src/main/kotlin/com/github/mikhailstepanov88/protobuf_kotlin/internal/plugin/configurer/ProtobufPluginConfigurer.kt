7bc05d65eb3bb08c66f2afa8aa6330989e57dcbe
Michael Stepanov <stepanovmixail@gmail.com>
2018-10-03 22:50:09 +0300

    Implement version 0.1 of protobuf kotlin plugin

--- src/main/kotlin/com/github/mikhailstepanov88/protobuf_kotlin/internal/plugin/configurer/ProtobufPluginConfigurer.kt
+++ src/main/kotlin/com/github/mikhailstepanov88/protobuf_kotlin/internal/plugin/configurer/ProtobufPluginConfigurer.kt
@@ -1,13 +1,9 @@
 package com.github.mikhailstepanov88.protobuf_kotlin.internal.plugin.configurer
 
 import com.google.protobuf.gradle.ExecutableLocator
-import com.google.protobuf.gradle.GenerateProtoTask
-import com.google.protobuf.gradle.GenerateProtoTask.PluginOptions
-import com.google.protobuf.gradle.ProtobufConfigurator.GenerateProtoTaskCollection
 import com.google.protobuf.gradle.ProtobufConvention
 import com.google.protobuf.gradle.ProtobufPlugin
 import groovy.lang.Closure
-import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.internal.Cast.uncheckedCast
 
@@ -47,8 +43,6 @@ class ProtobufPluginConfigurer : PluginConfigurer<ProtobufConvention>() {
      */
     override fun ProtobufConvention.update() {
         protobuf.protoc(protocConfig())
-        protobuf.plugins(pluginsConfig())
-        protobuf.generateProtoTasks(generateProtoTasksConfig())
     }
 
     /**
@@ -58,44 +52,6 @@ class ProtobufPluginConfigurer : PluginConfigurer<ProtobufConvention>() {
      */
     private fun protocConfig() = lazyConfigureObject<ExecutableLocator> {
         it.artifact = "com.google.protobuf:protoc:3.6.1"
-    }
-
-    /**
-     * Get configuration of codegen plugins.
-     *
-     * @return configuration of codegen plugins.
-     */
-    private fun pluginsConfig() = lazyConfigureObject<NamedDomainObjectContainer<ExecutableLocator>> {
-        it.create("grpc", grpcConfig())
-    }
-
-    /**
-     * Get configuration of grpc codegen plugin.
-     *
-     * @return configuration of grpc codegen plugin.
-     */
-    private fun grpcConfig() = lazyConfigureObject<ExecutableLocator> {
-        it.artifact = "io.grpc:protoc-gen-grpc-java:1.15.0"
-    }
-
-    /**
-     * Get configuration of generateProto tasks.
-     *
-     * @return configuration of generateProto tasks.
-     */
-    private fun generateProtoTasksConfig() = lazyConfigureObject<GenerateProtoTaskCollection> {
-        it.all().forEach(this::configureGenerateProtoTask)
-    }
-
-    /**
-     * Configure generateProto task.
-     *
-     * @param task generateProto task for configure.
-     */
-    private fun configureGenerateProtoTask(task: GenerateProtoTask) {
-        task.plugins(lazyConfigureObject<NamedDomainObjectContainer<PluginOptions>> {
-            it.create("grpc")
-        })
     }
 
     /**
